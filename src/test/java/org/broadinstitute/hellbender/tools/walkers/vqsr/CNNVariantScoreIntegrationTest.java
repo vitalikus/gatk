c8f4b425f8efc37a4637bf5d4a140d687758a2d7
Chris Norman <cnorman@broadinstitute.org>
2018-02-28 11:11:22 -0500

    Reenable 2d tests. Increase test output fequency to keep travis happy.

--- src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/CNNVariantScoreIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/CNNVariantScoreIntegrationTest.java
@@ -71,7 +71,7 @@ public class CNNVariantScoreIntegrationTest extends CommandLineProgramTest {
     /**
      * Run the 2D Model on a small test VCF.
      */
-    @Test(groups = {"python"}, enabled = false)
+    @Test(groups = {"python"})
     public void testInference2d() throws IOException{
         final ArgumentsBuilder argsBuilder = new ArgumentsBuilder();
         argsBuilder.addArgument(StandardArgumentDefinitions.VARIANT_LONG_NAME, inputVCF)
--- src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/CNNVariantTrainIntegrationTest.java
+++ src/test/java/org/broadinstitute/hellbender/tools/walkers/vqsr/CNNVariantTrainIntegrationTest.java
@@ -29,7 +29,7 @@ public class CNNVariantTrainIntegrationTest extends CommandLineProgramTest {
         runCommandLine(argsBuilder);
     }
 
-    @Test(groups = {"python"}, enabled = false)
+    @Test(groups = {"python"})
     public void testTrainingReadModel() throws IOException{
         CNNVariantWriteTensorsIntegrationTest wt = new CNNVariantWriteTensorsIntegrationTest();
         wt.generateReadTensors();
